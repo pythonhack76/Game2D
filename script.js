77371560c1b40d953ff190cd3226b78a5c91abaa
Python The Hack <lrulvoni@gmail.com>
2022-10-24 21:32:36 +0200

    update keyboard

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ window.addEventListener('load', function() {
     canvas.height = 500;
 
     class InputHandler {
+        constructor(game){
+            this.game = game;
+            window.addEventListener('keydown', function(e){
+                if(e.key === 'ArrowUp'){
+                    this.game.keys.push(e.key);
 
+                }
+                        console.log(e.key);
+            });
+        }
     }
 
     class Projectile {
@@ -19,6 +28,21 @@ window.addEventListener('load', function() {
 
     class Player {
 
+        constructor(game) {
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+
+        update() {
+                this.y += this.speedY;
+        }
+        draw(context) {
+                    context.fillRect(this.x, this.y, this.width, this.height);
+        }
 
     }
 
@@ -42,7 +66,31 @@ class UI {
 }
 
 class Game {
+    constructor(width, height){
+        this.width = width; 
+        this.height = height;
+        this.player = new Player(this); 
+        this.input = new InputHandler(this);
+        this.keys = [];
+    }
+    update() {
+        this.player.update();
+    }
+    draw(context){
+        this.player.draw(context);
+    }
 
 }
 
-})
+const game = new Game(canvas.width, canvas.height); 
+//animation loop 
+function animate(){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    game.update();
+    game.draw(ctx);
+    requestAnimationFrame(animate); 
+}
+
+animate();
+
+});
