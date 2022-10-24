ba055091c95d0724df8126250c96df53fd82542d
Python The Hack <lrulvoni@gmail.com>
2022-10-24 20:53:18 +0200

    update moving player

---
+++ script.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', function() {
+    //setup canvas 
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500; 
+    canvas.height = 500;
+
+    class InputHandler {
+
+    }
+
+    class Projectile {
+
+    }
+
+    class Particle {
+
+    }
+
+    class Player {
+
+
+    }
+
+    class Enemy {
+
+
+    }
+
+    class Layer {
+
+    }
+
+    class Background {
+
+    }
+
+
+class UI {
+
+
+}
+
+class Game {
+
+}
+
+})
