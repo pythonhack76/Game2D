16404b79e147a46a8228f9019b9b42f0ade249f5
Python The Hack <lrulvoni@gmail.com>
2022-10-24 21:53:40 +0200

    manage projectiles

--- script.js
+++ script.js
@@ -8,12 +8,20 @@ window.addEventListener('load', function() {
     class InputHandler {
         constructor(game){
             this.game = game;
-            window.addEventListener('keydown', function(e){
-                if(e.key === 'ArrowUp'){
+            window.addEventListener('keydown', e => {
+                if(( (e.key === 'ArrowUp') || 
+                    (e.key === 'ArrowDown')
+                ) && this.game.keys.indexOf(e.key) === -1) {
                     this.game.keys.push(e.key);
 
                 }
-                        console.log(e.key);
+                        console.log(this.game.keys);
+            });
+            window.addEventListener('keyup', e => {
+                if (this.game.keys.indexOf(e.key) > -1){
+                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1); 
+                }
+                console.log(this.game.keys);
             });
         }
     }
